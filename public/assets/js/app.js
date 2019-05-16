1b119b202c4fc9181659a8bafe4fb677362e3c7b
Chris Armata <chris.armata@gmail.com>
2019-05-16 00:45:04 -0400

    Attach burger ids, add devour functionality

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,38 @@
 var addBurger = () => {
   var data = $("form").serializeArray();
-  console.log(data);
   $.post("/api/burger", data).then(res => {
-    var row = $("<tr>").html(
-      `<td>${
-        data[0].value
-      }</td><td><button class='btn btn-outline-danger btn-sm devour'>Devour It!</button></td><td></td>`
-    );
+    var row = $("<tr>")
+      .attr("data-id", res.insertId)
+      .html(
+        `<td>${
+          data[0].value
+        }</td><td><button class='btn btn-outline-danger btn-sm devour'>Devour It!</button></td><td></td>`
+      );
     $("#burgers tbody").append(row);
     $("#burger_name").val("");
+  });
+};
+
+var devourBurger = btn => {
+  var row = $(btn.target)
+    .parent()
+    .parent();
+  var id = $(row).data("id");
+  var name = $(`tr[data-id=${id}]`)
+    .children()
+    .first()
+    .text();
+  var data = {
+    id: id,
+    devoured: 1
+  };
+
+  $.ajax({
+    url: `/api/burger/${id}`,
+    method: "PUT",
+    data: data
+  }).then(res => {
+    $(row).html(`<td></td><td></td><td>${name}</td>`);
   });
 };
 
@@ -16,5 +40,8 @@ $(document).ready(() => {
   $("form").submit(e => {
     e.preventDefault();
     addBurger();
+  });
+  $(document).on("click", ".devour", e => {
+    devourBurger(e);
   });
 });
